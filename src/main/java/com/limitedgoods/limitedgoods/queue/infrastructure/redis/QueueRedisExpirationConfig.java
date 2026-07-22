e0911f9722d34248fa528b0d22cb61399cd18393
sungjin <sungjinny5@naver.com>
2026-07-22 22:26:32 +0900

    refactoring : Common에서 Redis를 의존하고 있기에, common에서 분리하여 관리

--- src/main/java/com/limitedgoods/limitedgoods/queue/infrastructure/redis/QueueRedisExpirationConfig.java
+++ src/main/java/com/limitedgoods/limitedgoods/queue/infrastructure/redis/QueueRedisExpirationConfig.java
@@ -1,4 +1,4 @@
-package com.limitedgoods.limitedgoods.common.config;
+package com.limitedgoods.limitedgoods.queue.infrastructure.redis;
 
 import com.limitedgoods.limitedgoods.queue.listener.AdmissionTokenExpiredListener;
 import lombok.RequiredArgsConstructor;
@@ -10,19 +10,24 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 @RequiredArgsConstructor
-public class RedisExpirationConfig {
+public class QueueRedisExpirationConfig {
 
     private final RedisConnectionFactory connectionFactory;
     private final AdmissionTokenExpiredListener admissionTokenExpiredListener;
 
     @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer() {
+    public RedisMessageListenerContainer
+    queueRedisMessageListenerContainer() {
+
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
         container.setConnectionFactory(connectionFactory);
+
         container.addMessageListener(
                 admissionTokenExpiredListener,
-                new PatternTopic("__keyevent@0__:expired")
+                new PatternTopic("__keyevent@*__:expired")
         );
+
         return container;
     }
 }
